8b473dbaf2cfb404fd01c1dbf62ca85849d2b51c
AlexanderKlimov753 <99791069+AlexanderKlimov753@users.noreply.github.com>
2022-05-24 16:26:11 +0800

    Add files via upload

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand">Marvel</a>
+          <form class="d-flex">
+            <input class="form-control me-2" 
+              type="search" 
+              placeholder="Поиск..." 
+              aria-label="Search"
+              v-model="search"
+              @input="characterSearch(search)"
+              >
+            <button 
+              class="btn btn-outline-light" 
+              type="submit"
+               @click="()=>{search=''; changeSearch(search)}"
+              >
+              Все
+            </button>
+          </form>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader",
+        props: ["characterSearch"],
+        data() {
+            return {
+                search: "",
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppModal.vue
@@ -0,0 +1,51 @@
+<template>
+    
+     <div class="modal fade" id="Modal_1" tabindex="-1" aria-labelledby="#exampleModalLabel1" aria-hidden="true">
+             <div class="modal-dialog">
+                 <div class="modal-content">
+                 <div class="modal-header">
+                 <h5 class="modal-title" id="exampleModalLabel1">{{character.name}}</h5>
+                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+             <div class="modal-body">
+
+              
+
+                <img :src="character.thumbnail"
+                     :alt="character.name" 
+                     class="img-fluid rounded-start">
+
+                <div>
+                  <p class ="text-muted">{{character.modified}}</p>
+                  <h5>Описание:</h5>
+                  <p>
+                    {{character.description}}
+                  </p>
+                  <h5>Комиксы:</h5>
+                   <ul>
+                    <li v-for="(el, idx) in character.comics"
+                        :key="idx"
+                    >{{el.name}}</li> 
+                    
+                   </ul>
+                </div>
+                </div>
+             <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+             </div>
+             </div>
+            </div>
+            </div>
+
+</template>
+
+<script>
+    export default {
+        name: "AppModal",
+        props: ["character"]
+    }
+</script>
+
+<style scoped>
+
+</style>
