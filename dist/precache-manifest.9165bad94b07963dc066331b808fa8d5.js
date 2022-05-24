bacc50864c0e8728af5519f929cb679f2e643e73
AlexanderKlimov753 <99791069+AlexanderKlimov753@users.noreply.github.com>
2022-05-24 16:25:03 +0800

    Add files via upload

---
+++ dist/precache-manifest.9165bad94b07963dc066331b808fa8d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259bc0a5e4fb4c99e3476dc7a59ec16d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdf8de80d9d3748f9a56",
+    "url": "/js/app.522d2895.js"
+  },
+  {
+    "revision": "9b8b26c6a887e18dbee6",
+    "url": "/js/chunk-vendors.e701a922.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
